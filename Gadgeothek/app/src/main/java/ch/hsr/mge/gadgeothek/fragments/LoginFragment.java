01376db15c9703096abfa01def73752956625328
Urs Forrer <mail@ursforrer.ch>
2016-10-12 16:38:00 +0200

    Implement the buttons on the StartFragment
    Implement the buttons on the StartFragment

--- Gadgeothek/app/src/main/java/ch/hsr/mge/gadgeothek/fragments/LoginFragment.java
+++ Gadgeothek/app/src/main/java/ch/hsr/mge/gadgeothek/fragments/LoginFragment.java
@@ -2,22 +2,35 @@ package ch.hsr.mge.gadgeothek.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 import ch.hsr.mge.gadgeothek.*;
+import ch.hsr.mge.gadgeothek.service.*;
 
 /**
  * Created by Urs Forrer on 10.10.2016.
  */
 
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.login_fragment, container, false);
         //root.findViewById(R.id.nextButton).setOnClickListener(this);
         //getActivity().setTitle("Login");
         return root;
+    }
+
+    @Override
+    public void onClick (View v) {
+        TextInputEditText textInputEditTextMail = (TextInputEditText) v.findViewById(R.id.editTextLoginMail);
+        String mail = textInputEditTextMail.getText().toString();
+
+        TextInputEditText textInputEditTextPassword = (TextInputEditText) v.findViewById(R.id.editTextLoginPassword);
+        String password = textInputEditTextPassword.getText().toString();
+
     }
 }
--- Gadgeothek/app/src/main/java/ch/hsr/mge/gadgeothek/fragments/StartFragment.java
+++ Gadgeothek/app/src/main/java/ch/hsr/mge/gadgeothek/fragments/StartFragment.java
@@ -3,6 +3,7 @@ package ch.hsr.mge.gadgeothek.fragments;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +13,37 @@ import ch.hsr.mge.gadgeothek.*;
  * Created by Urs Forrer on 10.10.2016.
  */
 
-public class StartFragment extends Fragment {
+public class StartFragment extends Fragment implements View.OnClickListener {
+
+    NavigationView navView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.start_fragment, container, false);
-        //root.findViewById(R.id.nextButton).setOnClickListener(this);
+        root.findViewById(R.id.loginbutton_start).setOnClickListener(this);
+        root.findViewById(R.id.regbutton_start).setOnClickListener(this);
+
+        navView = (NavigationView) getActivity().findViewById(R.id.navigation_view);
+
+        //Sollen die OnClickLister mit setOnClickListener hier hier oder in die untere onClick Methode?
+
         return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        // Behandlung der OnClick-Methode hier jeweils in
+
+        switch (v.getId()) {
+            case R.id.loginbutton_start:
+                navView.setCheckedItem(R.id.drawerLogin);
+                getFragmentManager().beginTransaction().replace(R.id.content, new LoginFragment()).commit();
+                break;
+            case R.id.regbutton_start:
+                navView.setCheckedItem(R.id.drawerReg);
+                getFragmentManager().beginTransaction().replace(R.id.content, new RegistrationsFragment()).commit();
+                break;
+        }
     }
 }
